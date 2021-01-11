7293ff2e3faaf635cdc602f6f8cf8010daecfb80
firra.andriani22@gmail.com <firraandriani22@gmail.com>
2021-01-11 13:57:27 +0700

    modifikasi login

--- js/daftar 2.js
+++ js/daftar 2.js
@@ -1,6 +1,7 @@
  function validateForm() {
     var namaDep = document.getElementById('namaDep');
     var namaBel = document.getElementById('namaBel');
+    var tanggal = document.getElementById('tanggal');
 
     if (!namaDep.value) {
         alert("Nama Depan tidak boleh kosong");
@@ -10,6 +11,11 @@
     else if (!namaBel.value) {
         alert("Nama Belakang tidak boleh kosong");
         namaBel.focus();
+    }
+
+    else if (tanggal.value == "") {
+        alert("Tanggal Lahir tidak boleh kosong");
+        tanggal.focus();
     }
 
     else {
